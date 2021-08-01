03d9a488cf53d9c67d823acc3302272ac04473db
Lazy-AL <a_lazashvili@cu.edu.ge>
2021-08-01 14:39:18 +0400

    feat: created moon

--- main.js
+++ main.js
@@ -62,6 +62,19 @@ const me = new THREE.Mesh(
 );
 scene.add(me);
 
+// moon
+const moonTexture = new THREE.TextureLoader().load('./pictures/moon.jpg');
+const normalTexture = new THREE.TextureLoader().load('./pictures/normal.jpg');
+
+const moon = new THREE.Mesh(
+   new THREE.SphereGeometry(3,32,32),
+   new THREE.MeshStandardMaterial({
+       map: moonTexture,
+       normalMap:normalTexture
+   })
+);
+scene.add(moon);
+
 function animate() {
     requestAnimationFrame( animate);
 
