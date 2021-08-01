ae021aab961635428e2e5216f45fa40817fcf11c
Lazy-AL <a_lazashvili@cu.edu.ge>
2021-08-01 14:02:45 +0400

    feat: lighthelper and gridhelper

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import './styles/style.css'
 
 import * as THREE from 'three'
-
+import  {OrbitControls} from  'three/examples/jsm/controls/OrbitControls';
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -19,10 +19,20 @@ renderer.render(scene,camera);
 
 // creating object with material and shape
 const geometry = new THREE.TorusGeometry(10 , 3 , 16, 100)
-const material = new THREE.MeshBasicMaterial({ color: 0xFF6347,wireframe:true});
+const material = new THREE.MeshStandardMaterial({ color: 0xFF6347});
 const torrus = new THREE.Mesh(geometry,material);
 
 scene.add(torrus)
+
+// lighting for the object
+const pointLight = new THREE.PointLight(0xffffff)
+pointLight.position.set(5,5,5)
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(pointLight,ambientLight)
+
+const lightHelper = new THREE.PointLightHelper(pointLight)
+const gridHelper = new THREE.GridHelper(200,50);
+scene.add(lightHelper,gridHelper)
 
 function animate() {
     requestAnimationFrame( animate);
